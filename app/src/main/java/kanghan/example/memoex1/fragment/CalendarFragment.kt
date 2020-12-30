4b8439855b05a0b8521f8caa967febbfa12e78bf
sinki6 <sinki6@naver.com>
2020-12-30 21:10:15 +0900

    20201227 folder, calendar

--- app/src/main/java/kanghan/example/memoex1/fragment/CalendarFragment.kt
+++ app/src/main/java/kanghan/example/memoex1/fragment/CalendarFragment.kt
@@ -11,11 +11,12 @@ import com.skyhope.eventcalenderlibrary.model.Event
 import kanghan.example.memoex1.R
 import kanghan.example.memoex1.utils.MySharedPreferences
 import kotlinx.android.synthetic.main.fragment_calendar.*
+import ru.cleverpumpkin.calendar.CalendarDate
+import ru.cleverpumpkin.calendar.CalendarView
 import java.util.*
 
 class CalendarFragment : Fragment() {
 
-    private lateinit var calendarEvent: CalenderEvent
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,16 +27,68 @@ class CalendarFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        calendarEvent = calender_event
+
+        setUpCalendar()
+
+        val indicators: List<CalendarView.DateIndicator> = getDatesIndicators()
+        calendarView.datesIndicators = indicators
+
         getMemoData()
+
+    }
+    private fun setUpCalendar() {
+        val calendarView = calendar_view
+        val calendar = Calendar.getInstance()
+
+        //Initial date
+        //calendar.set(2018, Calendar.JUNE, 1)
+        val initialDate = CalendarDate(calendar.time)
+
+        //Minimum available date
+        calendar.set(2018,Calendar.JANUARY,1)
+        val minDate = CalendarDate(calendar.time)
+
+        //Maximum availabe date
+        calendar.set(2040, Calendar.DECEMBER, 31)
+        val maxDate = CalendarDate(calendar.time)
+
+        //List of preselected dates that will be initially selected
+        //val preselectedDates: List<CalendarDate> =getPreselectedDates()
+
+        val firstDayOfWeek = java.util.Calendar.SUNDAY
+
+        calendarView.setupCalendar(
+                initialDate = initialDate,
+                minDate = minDate,
+                maxDate = maxDate,
+                selectionMode = CalendarView.SelectionMode.NONE,
+                selectedDates = emptyList(),
+                firstDayOfWeek = firstDayOfWeek,
+                showYearSelectionView = true
+        )
+
     }
 
     private fun getMemoData() {
+
+        val indicators: List<CalendarView.DateIndicator> = getDatesIndicators()
+        calendarView.datesIndicators = indicators
+
+//        interface DateIndicator {
+//            val date: CalendarDate // indicator date
+//            val color: Int // indicator color
+//        }
+
         val items = MySharedPreferences.LoadData(context!!)
         items.forEach {
-            val event = Event(it.savedTimeInMills, it.memoTitle, Color.RED)
-            calendarEvent.addEvent(event)
+            MutableList<CalendarView.DateIndicator>().add( CalendarView.DateIndicator(it.savedTimeInMills, it.color))
         }
+    }
+    class DateIndicatorMemo: CalendarView.DateIndicator{
+        override val color: Int
+            get() = TODO("Not yet implemented")
+        override val date: CalendarDate
+            get() = TODO("Not yet implemented")
     }
 
     companion object {
